458e5f48010935c5253adc9ecc88d92e6e85851a
fy <wanfengiii@163.com>
2022-06-24 18:34:48 +0800

    test

--- testpart1/test-part3/src/main/java/Test.java
+++ testpart1/test-part3/src/main/java/Test.java
@@ -2,6 +2,7 @@ public class Test {
     private static String A = "a";
     private static String B = "b";
     private static String D = "d";
+    private static String S = "s";
 
     public static void testA() {
         System.out.println(A);
